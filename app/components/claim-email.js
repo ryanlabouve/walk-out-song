ffec9a46d47001b15d388c720cc60441c422765e
Ryan LaBouve <imryanlabouve@gmail.com>
2020-02-27 17:32:00 -0800

    asdf'

--- app/components/claim-email.js
+++ app/components/claim-email.js
@@ -34,7 +34,6 @@ export default class ClaimEmailComponent extends Component {
 
       if (getProfile.length) {
         profile = getProfile.firstObject;
-        debugger;
       }
 
       yield profile.save();
